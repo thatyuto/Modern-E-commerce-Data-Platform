b20b36674fcc33f4496bcaa5203a4519fb12817d
thatyuto <thatyuto@outlook.com>
2026-04-13 18:45:16 +0800

    feat: window function: SUM()

---
+++ leetcode_sql/1204. Last Person to Fit in the Bus.sql
@@ -0,0 +1,14 @@
+-- https://leetcode.cn/problems/last-person-to-fit-in-the-bus/description/?envType=study-plan-v2&envId=sql-free-50
+
+# Write your MySQL query statement below
+
+SELECT person_name
+FROM (
+    SELECT 
+        person_name,
+        SUM(weight) OVER (ORDER BY turn) AS total_weight 
+    FROM Queue
+) t
+WHERE total_weight <= 1000
+ORDER BY total_weight DESC
+LIMIT 1;
