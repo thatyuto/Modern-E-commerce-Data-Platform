1f3e2470af3ac52e5453a5115b4ba49fc89db0b3
thatyuto <thatyuto@outlook.com>
2026-04-14 18:13:35 +0800

    feat: create products table

---
+++ olist_analysis/sql/ddl/create_products_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE olist_raw.products (
+	product_id TEXT PRIMARY KEY,
+	product_category_name TEXT,
+	product_name_length INT,
+	product_description_length INT,
+	product_photos_qty INT,
+	product_weight_g INT,
+	product_length_cm INT,
+	product_height_cm INT,
+	product_width_cm INT
+);
