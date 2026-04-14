a7126174c082d0d0937c0d4ad7c36965a38d76f8
thatyuto <thatyuto@outlook.com>
2026-04-14 21:32:44 +0800

    feat: create geolocation table

---
+++ olist_analysis/sql/ddl/geolocation.sql
@@ -0,0 +1,7 @@
+CREATE TABLE olist_raw.geolocation (
+	geolocation_zip_code_prefix INT,
+	geolocation_lat DECIMAL(10,8),
+	geolocation_lng DECIMAL(10,8),
+	geolocation_city TEXT,
+	geolocation_state TEXT
+);
