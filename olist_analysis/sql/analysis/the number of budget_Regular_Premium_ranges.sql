851d346f31bdcc19e162d20e9451c861f5f1c8bb
thatyuto <thatyuto@outlook.com>
2026-04-18 15:42:21 +0800

    calculate the number of Budget, Regular and Premium ranges

---
+++ olist_analysis/sql/analysis/the number of budget_Regular_Premium_ranges.sql
@@ -0,0 +1,6 @@
+--  calculate the number of Budget, Regular and Premium ranges
+
+SELECT SUM(CASE WHEN price<49.9 THEN 1 ELSE 0 END) AS Budget_num,
+	   SUM(CASE WHEN price BETWEEN 49.9 AND 109 THEN 1 ELSE 0 END) AS Regular,
+	   SUM(CASE WHEN price>109 THEN 1 ELSE 0 END) AS Premium
+FROM olist_raw.order_items;
