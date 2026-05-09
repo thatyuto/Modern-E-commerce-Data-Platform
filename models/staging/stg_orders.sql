0ab1e25b607d685c0dd349a01e891339a521eb6a
yuto wong <thatyuto@outlook.com>
2026-05-09 08:16:27 +0000

    renamed the fields in orders table and standardize the data type

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -13,7 +13,8 @@ renamed as (
         cast(order_purchase_timestamp as timestamp) as purchased_at,
         cast(order_approved_at as timestamp) as approved_at,
         cast(order_delivered_carrier_date as timestamp) as delivered_to_carrier_at,
-        cast(order_delivered_customer_date as timestamp) as delivered_to_customer_at,
+        cast(order_delivered_customer_date as 
+        timestamp) as delivered_to_customer_at,
         cast(order_estimated_delivery_date as timestamp) as estimated_delivery_at
     from source 
 )
