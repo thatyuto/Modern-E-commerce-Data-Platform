62079ce8b6f10be4d454a98bdaf7901c785c6a10
yuto wong <thatyuto@outlook.com>
2026-05-11 10:26:33 +0000

    write fact_orders table

---
+++ models/marts/fact_orders.sql
@@ -0,0 +1,43 @@
+with orders as (
+    select * from {{ref('stg_orders')}}
+),
+-- solve fan out problem
+order_payments as (
+    select order_id,
+           sum(payment_value) as total_payment_value
+           from {{ref('stg_payments')}}
+           group by 1
+),
+-- preprocess the amount of product
+order_items_summary as (
+    select order_id,
+           sum(price) as total_item_price,
+           sum(freight_value) as total_freight_value,
+           count(product_id) as total_item_count
+    from {{ref('stg_order_items')}}
+    group by 1
+),
+final as (
+    select 
+        o.order_id,
+        o.customer_id,
+        o.order_status,
+        o.purchased_at,
+        o.approved_at,
+        o.delivered_to_customer_at,
+        
+        -- 核心度量值 (Measures)
+        -- coalesce(a,b), 如果a为NULL，则设置为b
+        coalesce(p.total_payment_value, 0) as total_payment_value,
+        coalesce(i.total_item_price, 0) as total_item_price,
+        coalesce(i.total_freight_value, 0) as total_freight_value,
+        coalesce(i.total_item_count, 0) as total_items_count,
+
+        -- 衍生派生字段：计算订单交付时效（天）
+        date_diff(o.delivered_to_customer_at, o.purchased_at, day) as delivery_time_days
+    from orders o  
+    left join order_payments p on o.order_id = p.order_id
+    left join order_items_summary i on o.order_id = i.order_id
+)
+
+select * from final
