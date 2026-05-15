f8b5c8ee9b4bfec419321342c071e90aecfb5872
yuto wong <thatyuto@outlook.com>
2026-05-15 16:28:51 +0000

    modify fact_order to an incremental model

--- dbt/models/marts/fact_orders.sql
+++ dbt/models/marts/fact_orders.sql
@@ -1,13 +1,24 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='order_id',
+        incremental_strategy='merge'
+    )
+
+}}
+
 with orders as (
     select * from {{ref('stg_orders')}}
 ),
--- solve fan out problem
+
+-- 修正点：在这里必须进行 sum 和 group by，确保一个 order_id 只有一行
 order_payments as (
     select order_id,
-           total_payment_value
+           sum(payment_value) as total_payment_value
     from {{ref('dim_payments')}}
+    group by 1
 ),
--- preprocess the amount of product
+
 order_items_summary as (
     select order_id,
            sum(price) as total_item_price,
@@ -16,6 +27,7 @@ order_items_summary as (
     from {{ref('stg_order_items')}}
     group by 1
 ),
+
 final as (
     select 
         o.order_id,
@@ -24,19 +36,27 @@ final as (
         o.purchased_at,
         o.approved_at,
         o.delivered_to_customer_at,
-        
-        -- 核心度量值 (Measures)
-        -- coalesce(a,b), 如果a为NULL，则设置为b
+
+       
+
         coalesce(p.total_payment_value, 0) as total_payment_value,
         coalesce(i.total_item_price, 0) as total_item_price,
         coalesce(i.total_freight_value, 0) as total_freight_value,
         coalesce(i.total_item_count, 0) as total_items_count,
-
-        -- 衍生派生字段：计算订单交付时效（天）
         date_diff(o.delivered_to_customer_at, o.purchased_at, day) as delivery_time_days
     from orders o  
     left join order_payments p on o.order_id = p.order_id
     left join order_items_summary i on o.order_id = i.order_id
+
+    -- 杀手锏：最后的唯一性屏障 : 如果存在重复order_id的数据，只取一条。
+    -- 但这不是一个最优解，产生多条数重复order_id的原因是join产生的fan-out问题
+    -- 因此强制的只取一条数据意味着，会丢失大量有用数据，因此最优解是解决fan-out问题
+    qualify row_number() over(partition by o.order_id order by o.purchased_at desc) = 1
 )
 
 select * from final
+
+{% if is_incremental() %}
+    -- 增量过滤：只增加最近七天的数据，如果没有这一条，model会扫全表，消耗的资源爆炸，此时incremental形同虚设
+    where purchased_at >= (select timestamp_sub(max(updated_at),interval 7 day) from {{this}})
+{% endif%}
